c94101403fc383e82d0cf3629fc502790f8eb927
Gabriel <134400952+GabzinhoHD@users.noreply.github.com>
2025-11-30 14:57:01 -0300

    Update

--- (Aula-14)AtividadeFinal/Transportadora.java
+++ (Aula-14)AtividadeFinal/Transportadora.java
@@ -1,4 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Transportadora implements ImportacaoArquivos {
+    
+    private static final int tamanho = 10; // Constante para definir o tamanho
+    
     /* Atributos */
     private Encomenda[] vetorEncomendas;
     private EncomendaExp[] vetorEncomendasExp;
@@ -7,10 +13,52 @@ public class Transportadora implements ImportacaoArquivos {
 
     /* Metodos */
     public Transportadora(){ // Metodo Construtor
-        this.vetorEncomendas = new Encomenda[10];
-        this.vetorEncomendasExp = new EncomendaExp[10];
+        this.vetorEncomendas = new Encomenda[tamanho];
+        this.vetorEncomendasExp = new EncomendaExp[tamanho];
         this.qtdEncomendas = 0;
         this.qtdEncomendasExp = 0;
+    }
+
+    /* Metodos da Interface */
+
+    public String carregarConfiguracoes(String arqConfig){
+
+    }
+    
+    public String importarDados(String arqDadosEntrada){
+        try{ // Tenta ler o arquivo
+            BufferedReader entrada = new BufferedReader(new FileReader(arqDadosEntrada)); // Buffer de leitura
+            String linha = entrada.readLine();
+
+            while((linha = entrada.readLine()) != null){ // Laço para percorre toda a tabela de dados
+                String[] campos = linha.split(";");
+                String siglaEncom = campos[2]; // Pega o tipo de encomenda pela sigla
+
+                if(siglaEncom.equals("EN")){ // Se a sigla for "EN" e uma encomenda normal
+                    /* Registra os dados da encomenda Normal */
+                    Encomenda encom = new Encomenda();
+                    encom.setNumPedido(Integer.parseInt(campos[0]));
+                    encom.setDataPostagem(campos[1]);
+                    encom.setPeso(Float.valueOf(campos[4]));
+                    this.setEncomenda(encom); // Salva a encomenda normal
+                }
+                else if(siglaEncom.equals("EE")){ // Se entao for "EE" é uma encomenda expressa
+                    /* Registra os dados da encomenda Expressa */
+                    EncomendaExp encomExp = new EncomendaExp();
+                    encomExp.setNumPedido(Integer.parseInt(campos[0]));
+                    encomExp.setDataPostagem(campos[1]);
+                    encomExp.setPrazoEntrega(Integer.parseInt(campos[3]));
+                    encomExp.setPeso(Float.valueOf(campos[4]));
+                    encomExp.setFone(campos[5]);
+                    this.setEncomendaExp(encomExp); // Salva a encomenda expressa
+                }
+            }
+            entrada.close();
+            return "Sucesso ao importar todos os dados!"; // Retorna uma msg de sucesso se todo ocorrre bem
+        }
+        catch(Exception e){ // Se não conseguir mostra o erro
+            return "Erro ao ler arquivo, motivo: " + e.getMessage();
+        }
     }
 
     /* Getter, Setter: encomenda */
@@ -18,7 +66,7 @@ public class Transportadora implements ImportacaoArquivos {
         return vetorEncomendas[pos];
     }
     public void setEncomenda(Encomenda encom) {
-        if(qtdEncomendas < 10){
+        if(qtdEncomendas < tamanho){
             this.vetorEncomendas[qtdEncomendas] = encom;
             this.qtdEncomendas++;
         }
@@ -29,8 +77,8 @@ public class Transportadora implements ImportacaoArquivos {
         return vetorEncomendasExp[pos];
     }
     public void setEncomendaExp(EncomendaExp encomExp) {
-        if(qtdEncomendasExp < 10){
-            this.vetorEncomendasExp[qtdEncomendas] = encomExp;
+        if(qtdEncomendasExp < tamanho){
+            this.vetorEncomendasExp[qtdEncomendasExp] = encomExp;
             this.qtdEncomendasExp++;
         }
     }
@@ -51,4 +99,11 @@ public class Transportadora implements ImportacaoArquivos {
         this.qtdEncomendasExp = qtdEncomendasExp;
     }
     
+    public int getEstoqueLivreEncom(){
+        return tamanho - qtdEncomendas;
+    }
+
+    public int getEstoqueLivreEncomExp(){
+        return tamanho - qtdEncomendasExp;
+    }
 }
